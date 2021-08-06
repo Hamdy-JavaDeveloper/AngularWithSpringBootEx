d6a2d52deb7b2cee2b5ad9bad157ca429165411e
Administrator <Administrator@Hamdi>
2021-08-06 17:30:22 +0300

    commit all files

---
+++ AngularSpringBootEx/src/main/java/hamdy/angspring/com/AngularSpringBootExApplication.java
@@ -0,0 +1,13 @@
+package hamdy.angspring.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringBootExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringBootExApplication.class, args);
+	}
+
+}
---
+++ AngularSpringBootEx/src/test/java/hamdy/angspring/com/AngularSpringBootExApplicationTests.java
@@ -0,0 +1,13 @@
+package hamdy.angspring.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringBootExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
